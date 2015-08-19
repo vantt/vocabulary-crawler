9f42ab0226092dc396dac6249292184c0e77edc4
vantt <trantoanvan@gmail.com>
2015-08-19 21:57:48 +0700

    ADAS

---
+++ mp3.php
@@ -0,0 +1,17 @@
+<?php
+require './vendor/autoload.php';
+
+set_time_limit(0);
+
+while ($word = trim(read_and_delete_first_line(__DIR__.'/vocabulary.txt'))) {
+    $content = file_get_contents("https://ssl.gstatic.com/dictionary/static/sounds/de/0/{$word}.mp3");
+    file_put_contents(__DIR__."/mp3/{$word}.mp3", $content);
+}
+
+function read_and_delete_first_line($filename) {
+    $file = file($filename);
+    $output = $file[0];
+    unset($file[0]);
+    file_put_contents($filename, $file);
+    return $output;
+}
