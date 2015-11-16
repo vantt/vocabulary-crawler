b91bf123fc2bcf28c90e8b1346f1494dbc1fcafd
vantt <trantoanvan@gmail.com>
2015-11-16 20:35:16 +0700

    update

--- mp3.php
+++ mp3.php
@@ -8,9 +8,11 @@ while ($word = trim(read_and_delete_first_line(__DIR__.'/vocabulary.txt'))) {
     file_put_contents(__DIR__."/mp3/{$word}.mp3", $content);
 }
 
+echo 'xong';
+
 function read_and_delete_first_line($filename) {
     $file = file($filename);
-    $output = $file[0];
+    $output = isset($file[0]) ? $file[0] : NULL;
     unset($file[0]);
     file_put_contents($filename, $file);
     return $output;
